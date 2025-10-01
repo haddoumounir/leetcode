207c3154e2c14f7abff75cafa48ff9a6d6260c70
mounir haddou <mounirhaddou.mh@gmail.com>
2025-10-02 00:44:35 +0100

    Implement maxBottlesDrunk function to calculate the maximum number of water bottles that can be drunk based on the initial number of bottles and exchange rate

---
+++ 3100_Water_Bottles_II/Water_Bottles_II.c
@@ -0,0 +1,14 @@
+int maxBottlesDrunk(int numBottles, int numExchange) 
+{
+    int totalDrunk = numBottles;
+    int emptyBottles = numBottles;
+    while (emptyBottles >= numExchange) 
+    {
+        emptyBottles -= numExchange;
+        numExchange++;
+        totalDrunk++;
+        emptyBottles++;
+    }
+    
+    return (totalDrunk);
+}
