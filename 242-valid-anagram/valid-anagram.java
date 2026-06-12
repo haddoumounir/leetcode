c54448d7c0764b6445a54d7d7c97d4ab96dcb62e
mounir <mounirhaddou.mh@gmail.com>
2026-06-12 18:43:56 +0100

    Time: 16 ms (21.67%) | Memory: 46.3 MB (40.14%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+     public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map1 = new HashMap<>();
+        HashMap<Character,Integer> map2 = new HashMap<>();
+
+        if(s.length() != t.length())
+            return false;
+        for(int i = 0; i <  s.length(); i++)
+        {
+            map1.put(s.charAt(i),1+ map1.getOrDefault(s.charAt(i),0));
+        }
+        for(int i = 0; i <  t.length(); i++)
+        {
+            map2.put(t.charAt(i),1+ map2.getOrDefault(t.charAt(i),0));
+        }
+        return map1.equals(map2);
+    }
+    
+}
