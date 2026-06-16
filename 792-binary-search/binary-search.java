f74672e79446475d1079e1ce38cbf27c717446ce
mounir <mounirhaddou.mh@gmail.com>
2026-06-16 11:38:44 +0100

    Time: 0 ms (100.00%) | Memory: 48.2 MB (83.78%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,8 +1,5 @@
 class Solution {
     public int search(int[] nums, int target) {
-
-        // if(nums.length == 0)
-        //     return -1;
         int r = nums.length -1;
         int l = 0;
         while (l <=r)
