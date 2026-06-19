ee008063733d213ce7deb01a29f2b527961beeee
mounir <mounirhaddou.mh@gmail.com>
2026-06-19 15:57:12 +0100

    Time: 7 ms (51.94%) | Memory: 22.8 MB (61.30%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,17 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        seen = {}
+        seens = {}
+
         for i in range(len(nums)):
-            seen[nums[i]] = seen.get(nums[i],0) + 1
+            seens[nums[i]] = seens.get(nums[i],0) + 1
 
         pair = []
 
-        for n in seen:
-            pair.append([seen[n],n])
+        for n in seens:
+            pair.append([seens[n],n])
         pair.sort(reverse=True)
         result = []
-
         for i in range(k):
             result.append(pair[i][1])
+
+
         return result
